c81158474a65f86181a52452a8c20a0ccb67396b
DiegoLaguna-17 <diego.laguna@ucb.edu.bo>
2025-03-13 18:40:52 -0400

    Cambios

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI );
         console.log('Conexión exitosa a MongoDB');
     } catch (error) {
         console.error('Error de conexión:', error.message);
